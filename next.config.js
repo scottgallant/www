800e4c8a4e45b7bfbebb6c65008b607fe43dc6d1
Nolan Phillips <ncphillips.19@gmail.com>
2020-05-08 12:50:16 -0300

    add tina webpack helpers
    these are useful for testing changes to
    tina in this local site

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
-require('dotenv').config();
+const { aliasTinaDev } = require("@tinacms/webpack-helpers");
+
+require("dotenv").config();
 
 module.exports = {
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
+  },
+  webpack(config) {
+    if (process.env.TINA) {
+      let watch;
+      if (process.env.TINA_WATCH) {
+        watch = process.env.TINA_WATCH.split(",");
+      }
+      aliasTinaDev(config, process.env.TINA, watch);
+    }
+
+    return config;
   },
 };
